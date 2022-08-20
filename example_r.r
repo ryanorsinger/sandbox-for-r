6cfabc1ccdf9cf328f4b3b51dec272d8502e59fc
Ryan Orsinger <ryan.orsinger@gmail.com>
2022-08-20 18:29:07 -0500

    add clarifying comments

--- example_r.r
+++ example_r.r
@@ -3,10 +3,17 @@
     
 
 # see ?cars for details
+# This appears to bind a global variable without an assignment operation
 data(cars)
+
 # fit a linear regression of distance on speed
+# Interesting to be able to access this w/o an import
 m <- lm( dist ~ speed , data=cars )
+
 # estimated coefficients from the model
+# Nice that this is global/default w/o import
 coef(m)
+
 # plot residuals against speed
+# Nice that this is global/default w/o import
 plot( resid(m) ~ speed , data=cars )
